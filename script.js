5b25d062ca049d83b712463ad4fb2eebc6aa7594
Yordani <yalvarezb93@gmail.com>
2024-01-23 15:35:09 +0300

    Js 2

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let popUpShare = document.querySelector(".share");
 let popUp = document.querySelector(".pop-up");
 
+const changeShare = (toggle) => {
+    if (toggle) {
+        popUpShare.style.backgroundColor = 'var(--Desaturated-Dark-Blue)'
+        popUpShare.style.backgroundImage = `url('./images/icon-share-white.svg')`
+    } else {
+        popUpShare.style.backgroundColor = 'var(--Light-Grayish-Blue)'
+        popUpShare.style.backgroundImage = `url('./images/icon-share.svg')`
+    }
+}
 
-const showPopUp = (popUpShare.addEventListener("mouseover", () => {
+
+
+const showPopUp = (popUpShare.addEventListener("mouseenter", () => {
+    changeShare(true)
     popUp.classList.toggle("hidden");
 }))
 
 const hidePopUp = (popUp.addEventListener("mouseleave", () => {
+    changeShare(false)
     popUp.classList.toggle("hidden");
 }))
-
-const showPopUpShare = popUp.addEventListener('mouseover', () => {
-    popUpShare.classList.toggle('active')
-})
 
 
